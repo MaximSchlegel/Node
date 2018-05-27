67439586c6a3a00940bf270d969c9bce4737a332
Maxim <Maxim@DESKTOP-V7IL6S5.local>
2018-05-27 12:49:12 +0200

    Aufgabe 6 kleine verbesserung

--- Server.ts
+++ Server.ts
@@ -6,35 +6,100 @@ namespace Server {
         [key: string]: string;
     }
 
+    interface Studi {
+        name: string;
+        firstname: string;
+        matrikel: number;
+        age: number;
+        gender: boolean;
+        course: string;
+    }
+
+    interface Studis {
+        [matrikel: string]: Studi;
+    }
+
+    let studiHomoAssoc: Studis = {};
     let port: number = process.env.PORT;
     if (port == undefined)
-        port = 8100;
-
-    let server: Http.Server = Http.createServer();
-    server.addListener("listening", handleListen);
+        port = 8200;
+    let server: Http.Server = Http.createServer((_request: Http.IncomingMessage, _response: Http.ServerResponse) => {
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+    });
     server.addListener("request", handleRequest);
     server.listen(port);
 
-    function handleListen(): void {
-        console.log("Ich höre?");
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        console.log("Ich h�re Stimmen!");
+        let query: AssocStringString = Url.parse(_request.url, true).query;
+        console.log(query["command"]);
+        if (query["command"]) {
+            switch (query["command"]) {
+                case "insert":
+                    insert(query, _response);
+                    break;
+
+                case "refresh":
+                    refresh(_response);
+                    break;
+
+                case "search":
+                    search(query, _response);
+                    break;
+
+                default:
+                    error();
+            }
+        }
+        _response.end();
     }
 
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-        console.log("Ich höre Stimmen!");
+    function insert(query: AssocStringString, _response: Http.ServerResponse): void {
+        let obj: Studi = JSON.parse(query["data"]);
+        let _name: string = obj.name;
+        let _firstname: string = obj.firstname;
+        let matrikel: string = obj.matrikel.toString();
+        let _age: number = obj.age;
+        let _gender: boolean = obj.gender;
+        let _course: string = obj.course;
+        let studi: Studi;
+        studi = {
+            name: _name,
+            firstname: _firstname,
+            matrikel: parseInt(matrikel),
+            age: _age,
+            gender: _gender,
+            course: _course
+        };
+        studiHomoAssoc[matrikel] = studi;
+        _response.write("Daten empfangen");
+    }
 
-        let query: AssocStringString = Url.parse(_request.url, true).query;
-        let a: number = parseInt(query["a"]);
-        let b: number = parseInt(query["b"]);
+    function refresh(_response: Http.ServerResponse): void {
+        console.log(studiHomoAssoc);
+        for (let matrikel in studiHomoAssoc) {
+            let studi: Studi = studiHomoAssoc[matrikel];
+            let line: string = matrikel + ": ";
+            line += studi.course + ", " + studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre ";
+            line += studi.gender ? "(M)" : "(F)";
+            _response.write(line + "\n");
+        }
+    }
 
-        for (let key in query) 
-            console.log(query[key]);
+    function search(query: AssocStringString, _response: Http.ServerResponse): void {
+        let studi: Studi = studiHomoAssoc[query["searchFor"]];
+        if (studi) {
+            let line: string = query["searchFor"] + ": ";
+            line += studi.course + ", " + studi.name + ", " + studi.firstname + ", " + studi.age + " Jahre ";
+            line += studi.gender ? "(M)" : "(F)";
+            _response.write(line);
+        } else {
+            _response.write("No Match");
+        }
+    }
 
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
-
-        _response.write("Ich habe dich gehört<br/>");
-        _response.write("Das Ergebnis ist: " + (a + b));
-
-        _response.end();
+    function error(): void {
+        alert("Error");
     }
 }
